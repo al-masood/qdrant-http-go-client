29f0af30eb80e617251140eec314396a858bb4ae
Masood <masood@appscode.com>
2026-02-23 17:04:24 +0600

    initial structure
    Signed-off-by: Masood <masood@appscode.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/al-masood/qdrant-http-go-client
+
+go 1.25.5
---
+++ qdrant/shard_snapshot.go
@@ -0,0 +1,19 @@
+package qdrant
+
+import "context"
+
+func (c *Client) CreateShardSnapshot(ctx context.Context, collectionName string, shardID string) {
+
+}
+
+func (c *Client) ListShardSnapshot(ctx context.Context, collectionName string, shardID string) {
+
+}
+
+func (c *Client) DeleteShardSnapshot(ctx context.Context, collectionName string, shardID string) {
+
+}
+
+func (c *Client) RestoreShardSnapshot(ctx context.Context, collectionName string, shardID string, location string) {
+
+}
